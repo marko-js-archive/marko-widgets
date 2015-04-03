4784ff433e6b80166cbbaaec756760e9f5c39794
Pranav Jha <prjha@ebay.com>
2015-04-04 01:08:42 +0530

    removed cleanup function

--- test/client/util.js
+++ test/client/util.js
@@ -13,30 +13,7 @@ require('./fixtures/components/app-fixed-id/widget');
 
 require('chai').Assertion.includeStack = true;
 
-
-function cleanup() {
-
-    window.testData = {
-        widgets: {
-
-        },
-        addWidget: function(name, widget) {
-            var widgetList = window.testData.widgets[name] || (window.testData.widgets[name] = []);
-            widgetList.push(widget);
-        }
-    };
-
-    var targetEl = document.getElementById('target');
-    if (targetEl) {
-        targetEl.parentNode.removeChild(targetEl);
-    }
-
-    var div = document.createElement('div');
-    div.id = 'target';
-    document.body.appendChild(div);
-}
-
-function triggerMouseEvent(el, type) {
+exports.triggerMouseEvent = function (el, type) {
     var ev = document.createEvent("MouseEvent");
     ev.initMouseEvent(
         type,
@@ -47,10 +24,7 @@ function triggerMouseEvent(el, type) {
         0 /*left*/, null
     );
     el.dispatchEvent(ev);
-}
-
-exports.cleanup = cleanup;
-exports.triggerMouseEvent = triggerMouseEvent;
+};
 
 Object.defineProperty(
     exports,

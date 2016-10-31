4f13d8aac197dd405a1e18929821a47901f14e1a
Patrick Steele-Idem <pnidem@gmail.com>
2016-10-31 12:27:01 -0600

    Lasso v2 compatibility

--- taglib/TransformHelper/getClientWidgetPath.js
+++ taglib/TransformHelper/getClientWidgetPath.js
@@ -1,9 +1,15 @@
 var tryRequire = require('try-require');
 var raptorModulesResolver = tryRequire('raptor-modules/resolver', require);
+var lasso = tryRequire('lasso', require);
+var resolveFrom = require('resolve-from');
 
 module.exports = function getClientWidgetPath(targetModuleFile) {
-    if (raptorModulesResolver) {
-        var from = this.template.dirname;
+    var from = this.template.dirname;
+
+    if (lasso && lasso.getClientPath) {
+        var targetPath = resolveFrom(from, targetModuleFile);
+        return lasso.getClientPath(targetPath);
+    } else if (raptorModulesResolver) {
         var resolved = raptorModulesResolver.resolveRequire(targetModuleFile, from);
         return resolved.logicalPath;
     } else {
